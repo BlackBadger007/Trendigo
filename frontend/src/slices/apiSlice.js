f3ad2707ab5fb2b15539e0511b647e028a3ad70e
Aditya Malik <adityamalik2058@gmail.com>
2024-10-23 14:50:34 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi , fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({baseUrl : "http://localhost:5000" , credentials : 'include'})
+// const baseQuery = fetchBaseQuery({baseUrl : "http://localhost:5000" , credentials : 'include'})
+const baseQuery = fetchBaseQuery({baseUrl : "https://trendigo-75fp.onrender.com/" , credentials : 'include'})
 
 export const apiSlice = createApi({
     baseQuery,
