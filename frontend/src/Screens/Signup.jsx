7ee138694dc2072672bc839019dffb48c7099e48
Aditya Malik <adityamalik2058@gmail.com>
2024-10-23 14:42:47 +0530

    Update Signup.jsx

--- frontend/src/Screens/Signup.jsx
+++ frontend/src/Screens/Signup.jsx
@@ -47,7 +47,8 @@ const Signup = () => {
             toast("Password Mismatch")
             return
         }
-        const response = await axios.post("http://localhost:5000/auth/signup" , user , {withCredentials : true} )
+        // const response = await axios.post("http://localhost:5000/auth/signup" , user , {withCredentials : true} )
+        const response = await axios.post("https://trendigo-75fp.onrender.com/auth/signup" , user , {withCredentials : true} )
         console.log(response.data)
 
         if(response.data.message === "already"){
